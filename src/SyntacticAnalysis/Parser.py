b9c7ce2815b86d84dfc1d9891079840140a94605
Sam <samuelgardner101@gmail.com>
2023-12-19 02:33:18 +0000

    Begin porting parsing code

--- src/SyntacticAnalysis/Parser.py
+++ src/SyntacticAnalysis/Parser.py
@@ -1,2 +1,53 @@
+from __future__ import annotations
+from typing import Callable
+
+from SyntacticAnalysis.ParserRuleHandler import ParserRuleHandler
+from src.LexicalAnalysis.Tokens import Token, TokenType
+from src.SyntacticAnalysis.ParserRuleHandler import ParserRuleHandler
+from src.SemanticAnalysis.ASTs.Ast import *
+
+
+# Decorator that wraps the function in a ParserRuleHandler
+def parser_rule(rule: ParserRuleHandler.ParserRule) -> Callable[[], ParserRuleHandler]:
+    def wrapper(self) -> ParserRuleHandler:
+        return ParserRuleHandler(self, rule)
+    return wrapper
+
+
 class Parser:
-    ...
+    _tokens: List[Token]
+    _index: int
+
+    def current_pos(self) -> int:
+        return self._index
+
+    @parser_rule
+    def parse_eof(self) -> TokenAst:
+        p1 = self.parse_token(TokenType.TkEOF).parse_once()
+        return p1
+
+    @parser_rule
+    def parse_program(self) -> ProgramAst:
+        c1 = self.current_pos()
+        p1 = self.parse_module_prototype().parse_once()
+        p2 = self.parse_eof().parse_once()
+        return ProgramAst(c1, p1, p2)
+
+    @parser_rule
+    def parse_module_prototype(self) -> ModulePrototypeAst:
+        c1 = self.current_pos()
+        p1 = self.parse_annotation().parse_zero_or_more()
+        p2 = self.parse_token(TokenType.TkModule).parse_once()
+        p3 = self.parse_module_identifier().parse_once()
+        p4 = self.parse_module_member().parse_zero_or_more()
+        return ModulePrototypeAst(c1, p1, p2, p3, p4)
+
+    @parser_rule
+    def parse_module_identifier(self) -> ModuleIdentifierAst:
+        c1 = self.current_pos()
+        p1 = self.parse_identifier().parse_one_or_more(TokenType.TkDot)
+        return ModuleIdentifierAst(c1, p1)
+
+    @parser_rule
+    def parse_token(self, token_type: TokenType) -> TokenAst:
+        ...
--- src/SyntacticAnalysis/ParserRuleHandler.py
+++ src/SyntacticAnalysis/ParserRuleHandler.py
@@ -8,13 +8,13 @@ from src.SyntacticAnalysis.ParserAlternateRulesHandler import ParserAlternateRul
 from src.SyntacticAnalysis.ParserError import ParserError
 
 
-class ParserRuleHandler:
-    ParserRule = Callable[[], Ast]
+class ParserRuleHandler[T]:
+    ParserRule = Callable[[], T]
 
     _rule: ParserRule
     _parser: Parser
     _for_alternate: bool
-    _result: Optional[Ast]
+    _result: Optional[T]
 
     def __init__(self, parser: Parser, rule: ParserRule) -> None:
         self._parser = parser
@@ -22,11 +22,11 @@ class ParserRuleHandler:
         self._for_alternate = False
         self._result = None
 
-    def parse_once(self) -> Ast:
+    def parse_once(self) -> T:
         self._result = self._rule()
         return self._result
 
-    def parse_optional(self) -> Optional[Ast]:
+    def parse_optional(self) -> Optional[T]:
         parser_index = self._parser._index
         try:
             self._result = self._rule()
@@ -35,14 +35,14 @@ class ParserRuleHandler:
             self._parser._index = parser_index
             return None
 
-    def parse_zero_or_more(self, sep: TokenType = None) -> List[Ast]:
+    def parse_zero_or_more(self, sep: TokenType = None) -> List[T]:
         self._result = []
         while ast := self.parse_optional():
             self._result.append(ast)
             sep and self._parser.parse_token(sep).parse_once()
         return self._result
 
-    def parse_one_or_more(self, sep: TokenType = None) -> List[Ast]:
+    def parse_one_or_more(self, sep: TokenType = None) -> List[T]:
         self.parse_zero_or_more(sep)
         if not self._result:
             raise ParserError("Expected one or more.")
