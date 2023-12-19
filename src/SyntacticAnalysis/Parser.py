d30bb08f7673e053661de1bceb6e08754001f637
Sam <samuelgardner101@gmail.com>
2023-12-19 02:42:07 +0000

    Begin porting parsing code

--- src/SyntacticAnalysis/Parser.py
+++ src/SyntacticAnalysis/Parser.py
@@ -35,12 +35,33 @@ class Parser:
 
     @parser_rule
     def parse_module_prototype(self) -> ModulePrototypeAst:
+        """
+        [ModulePrototype] => [Annotation]* [Tok("mod")] [ModuleIdentifier] [ModuleMember]*
+
+        - [Annotation]*      => The list of token stream mutations to make to the module before it is parsed.
+        - [Tok("mod")]       => The 'mod' keyword.
+        - [ModuleIdentifier] => The identifier of the module.
+        - [ModuleMember]*    => The list of members that form the implementation of the module.
+
+        The ModulePrototype contains the members that form a single module. It is a "top-level" AST, because the
+        ProgramAst stores a ModulePrototypeAst instance directly.
+        """
         c1 = self.current_pos()
         p1 = self.parse_annotation().parse_zero_or_more()
         p2 = self.parse_token(TokenType.TkModule).parse_once()
         p3 = self.parse_module_identifier().parse_once()
         p4 = self.parse_module_member().parse_zero_or_more()
         return ModulePrototypeAst(c1, p1, p2, p3, p4)
+
+    @parser_rule
+    def parse_module_member(self) -> ModuleMemberAst:
+        p1 = self.parse_function_prototype().for_alt()
+        p2 = self.parse_class_prototype().for_alt()
+        p3 = self.parse_sup_prototype_normal().for_alt()
+        p4 = self.parse_sup_prototype_inheritance().for_alt()
+        p5 = self.parse_typedef_statement().for_alt()
+        p6 = (p1 | p2 | p3 | p4 | p5).parse_once()
+        return p6
 
     @parser_rule
     def parse_module_identifier(self) -> ModuleIdentifierAst:
