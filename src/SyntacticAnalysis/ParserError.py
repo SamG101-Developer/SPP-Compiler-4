f4f8aa789861a06bb978e10825dd39cee3fd9d2c
Sam <samuelgardner101@gmail.com>
2023-12-20 23:54:15 +0000

    List -> Set (makes more sense)

--- src/SyntacticAnalysis/ParserError.py
+++ src/SyntacticAnalysis/ParserError.py
@@ -1,12 +1,12 @@
-from typing import List
+from typing import Set
 from src.LexicalAnalysis.Tokens import TokenType
 
 
 class ParserError(Exception):
     pos: int
-    expected_tokens: List[str]
+    expected_tokens: Set[str]
 
     def __init__(self, *args) -> None:
         super().__init__(*args)
         self.pos = -1
-        self.expected_tokens = []
+        self.expected_tokens = set()
