e7c8eb850124218cb45d8536d30051fb8221bbf3
Sam <samuelgardner101@gmail.com>
2023-12-21 19:58:58 +0000

    Lex integers & floats differently

--- src/LexicalAnalysis/Tokens.py
+++ src/LexicalAnalysis/Tokens.py
@@ -103,13 +103,14 @@ class TokenType(Enum):
 
     # Don't change order of these (regex are matched in this order)
     # 0x12 must be HexDigits not DecDigits(0) then Identifier(x12)
+    LxRegex = r"r\".*\""
     LxIdentifier = r"[a-z][_a-zA-Z0-9]*"
     LxUpperIdentifier = r"[A-Z][_a-zA-Z0-9]*"
     LxBinDigits = r"0b[01]+"
     LxHexDigits = r"0x[0-9a-fA-F]+"
     LxDecDigits = r"[0-9]([0-9_]*[0-9])?"
+    LxDecFloat = r"[0-9]([0-9_]*[0-9])?\.[0-9]([0-9_]*[0-9])?"
     LxDoubleQuoteStr = r"\"[^\"]*\""
-    LxRegex = r"r\".*\""
     LxSingleLineComment = r"#.*"
     LxMultiLineComment = r"/\*.*\*/"
 
