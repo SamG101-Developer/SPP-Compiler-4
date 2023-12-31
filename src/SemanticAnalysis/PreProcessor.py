9c04b00813492cf584d4c685baeb56e6380c487f
Sam <samuelgardner101@gmail.com>
2023-12-31 18:12:14 +0000

    Rename method

--- src/SemanticAnalysis/PreProcessor.py
+++ src/SemanticAnalysis/PreProcessor.py
@@ -1,11 +1,7 @@
 from abc import ABC, abstractmethod
-from src.SemanticAnalysis.ASTs.Ast import ModulePrototypeAst, SupPrototypeAst
-
-
-PreProcessorContext = ModulePrototypeAst | SupPrototypeAst
 
 
 class PreProcessor(ABC):
     @abstractmethod
-    def process(self, text: str, context: PreProcessorContext) -> None:
+    def pre_process(self, context) -> None:
         pass
