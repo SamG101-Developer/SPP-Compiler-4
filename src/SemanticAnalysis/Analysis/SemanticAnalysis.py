ccefe22bc4f6b338b66659a4dffdd9b06ecbd056
Sam <samuelgardner101@gmail.com>
2024-01-02 02:26:23 +0000

    Fixed type annotation

--- src/SemanticAnalysis/Analysis/SemanticAnalysis.py
+++ src/SemanticAnalysis/Analysis/SemanticAnalysis.py
@@ -1,10 +1,10 @@
 from abc import ABC, abstractmethod
 from typing import Iterator
 
-from src.SemanticAnalysis.Symbols.Scopes import ScopeHandler
+from src.SemanticAnalysis.Symbols.Scopes import Scope
 
 
 class SemanticAnalysis(ABC):
     @abstractmethod
-    def do_semantic_analysis(self, s: Iterator[ScopeHandler]) -> None:
+    def do_semantic_analysis(self, s: Iterator[Scope]) -> None:
         pass
