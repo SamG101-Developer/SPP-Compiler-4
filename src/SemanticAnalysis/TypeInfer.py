ee925e5c4605cda2e33798b7811c241790ab6fff
Sam <samuelgardner101@gmail.com>
2024-01-03 21:38:39 +0000

    Add the `scope_handler: ScopeHandler` parameter

--- src/SemanticAnalysis/TypeInfer.py
+++ src/SemanticAnalysis/TypeInfer.py
@@ -1,7 +1,10 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
+
+from src.SemanticAnalysis.Symbols.Scopes import ScopeHandler
 
 
 class TypeInfer(ABC):
     @abstractmethod
-    def infer_type(self) -> TypeAst:
+    def infer_type(self, scope_handler: ScopeHandler) -> TypeAst:
         pass
