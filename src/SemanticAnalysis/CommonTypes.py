29e5bf07381a9af44e6ce7dd7ca2e71aa591f313
Sam <samuelgardner101@gmail.com>
2024-01-03 21:36:36 +0000

    Added some more types required for semantic analysis

--- src/SemanticAnalysis/CommonTypes.py
+++ src/SemanticAnalysis/CommonTypes.py
@@ -30,6 +30,16 @@ class CommonTypes:
         return TypeSingleAst(-1, [GenericIdentifierAst(-1, "Str", None)])
 
     @staticmethod
+    def rgx():
+        from src.SemanticAnalysis.ASTs.Ast import TypeSingleAst, GenericIdentifierAst
+        return TypeSingleAst(-1, [GenericIdentifierAst(-1, "Rgx", None)])
+
+    @staticmethod
+    def ctx():
+        from src.SemanticAnalysis.ASTs.Ast import TypeSingleAst, GenericIdentifierAst
+        return TypeSingleAst(-1, [GenericIdentifierAst(-1, "Ctx", None)])
+
+    @staticmethod
     def arr(elem_type):
         from src.SemanticAnalysis.ASTs.Ast import TypeSingleAst, GenericIdentifierAst, GenericArgumentGroupAst, GenericArgumentNormalAst, TokenAst
         from src.LexicalAnalysis.Tokens import TokenType
@@ -44,25 +54,25 @@ class CommonTypes:
         return TypeSingleAst(-1, [GenericIdentifierAst(-1, "Tuple", types)])
 
     @staticmethod
-    def fun_ref(return_type, param_types):
+    def fun_ref(return_type, param_types, pos: int = -1):
         from src.SemanticAnalysis.ASTs.Ast import TypeTupleAst, TypeSingleAst, GenericIdentifierAst, GenericArgumentGroupAst, GenericArgumentNormalAst, TokenAst
         from src.LexicalAnalysis.Tokens import TokenType
         return_type_generic = GenericArgumentNormalAst(-1, return_type)
         param_types_generic = GenericArgumentNormalAst(-1, TypeTupleAst(-1, TokenAst.dummy(TokenType.TkParenL), param_types, TokenAst.dummy(TokenType.TkParenR)))
-        return TypeSingleAst(-1, [GenericIdentifierAst(-1, "FunRef", GenericArgumentGroupAst(-1, TokenAst.dummy(TokenType.TkBrackL), [return_type_generic, param_types_generic], TokenAst.dummy(TokenType.TkBrackR)))])
+        return TypeSingleAst(pos, [GenericIdentifierAst(-1, "FunRef", GenericArgumentGroupAst(-1, TokenAst.dummy(TokenType.TkBrackL), [return_type_generic, param_types_generic], TokenAst.dummy(TokenType.TkBrackR)))])
 
     @staticmethod
-    def fun_mut(return_type, param_types):
+    def fun_mut(return_type, param_types, pos: int = -1):
         from src.SemanticAnalysis.ASTs.Ast import TypeTupleAst, TypeSingleAst, GenericIdentifierAst, GenericArgumentGroupAst, GenericArgumentNormalAst, TokenAst
         from src.LexicalAnalysis.Tokens import TokenType
         return_type_generic = GenericArgumentNormalAst(-1, return_type)
         param_types_generic = GenericArgumentNormalAst(-1, TypeTupleAst(-1, TokenAst.dummy(TokenType.TkParenL), param_types, TokenAst.dummy(TokenType.TkParenR)))
-        return TypeSingleAst(-1, [GenericIdentifierAst(-1, "FunMut", GenericArgumentGroupAst(-1, TokenAst.dummy(TokenType.TkBrackL), [return_type_generic, param_types_generic], TokenAst.dummy(TokenType.TkBrackR)))])
+        return TypeSingleAst(pos, [GenericIdentifierAst(-1, "FunMut", GenericArgumentGroupAst(-1, TokenAst.dummy(TokenType.TkBrackL), [return_type_generic, param_types_generic], TokenAst.dummy(TokenType.TkBrackR)))])
 
     @staticmethod
-    def fun_one(return_type, param_types):
+    def fun_one(return_type, param_types, pos: int = -1):
         from src.SemanticAnalysis.ASTs.Ast import TypeTupleAst, TypeSingleAst, GenericIdentifierAst, GenericArgumentGroupAst, GenericArgumentNormalAst, TokenAst
         from src.LexicalAnalysis.Tokens import TokenType
         return_type_generic = GenericArgumentNormalAst(-1, return_type)
         param_types_generic = GenericArgumentNormalAst(-1, TypeTupleAst(-1, TokenAst.dummy(TokenType.TkParenL), param_types, TokenAst.dummy(TokenType.TkParenR)))
-        return TypeSingleAst(-1, [GenericIdentifierAst(-1, "FunOne", GenericArgumentGroupAst(-1, TokenAst.dummy(TokenType.TkBrackL), [return_type_generic, param_types_generic], TokenAst.dummy(TokenType.TkBrackR)))])
+        return TypeSingleAst(pos, [GenericIdentifierAst(-1, "FunOne", GenericArgumentGroupAst(-1, TokenAst.dummy(TokenType.TkBrackL), [return_type_generic, param_types_generic], TokenAst.dummy(TokenType.TkBrackR)))])
