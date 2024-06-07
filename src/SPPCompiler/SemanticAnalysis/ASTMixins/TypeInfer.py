ee18917e5c8bb6459734c915b321ee569dbf59e9
Sam Gardner <samuelgardner101@gmail.com>
2024-06-07 04:06:52 +0100

    Change `Tuple[ConventionAst, TypeAst]` to `InferredType`

--- src/SPPCompiler/SemanticAnalysis/ASTMixins/TypeInfer.py
+++ src/SPPCompiler/SemanticAnalysis/ASTMixins/TypeInfer.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
+
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from typing import Tuple, Type
 
 from SPPCompiler.SemanticAnalysis.Utils.Scopes import ScopeHandler
@@ -7,5 +9,17 @@ from SPPCompiler.SemanticAnalysis.Utils.Scopes import ScopeHandler
 
 class TypeInfer(ABC):
     @abstractmethod
-    def infer_type(self, scope_handler: ScopeHandler, **kwargs) -> Tuple[Type[ConventionAst], TypeAst]:
+    def infer_type(self, scope_handler: ScopeHandler, **kwargs) -> InferredType:
         pass
+
+
+@dataclass(kw_only=True)
+class InferredType:
+    convention: Type[ConventionAst]
+    type: TypeAst
+
+    def symbolic_eq(self, other: InferredType, scope_handler: ScopeHandler) -> bool:
+        return self.convention == other.convention and self.type.symbolic_eq(other.type, scope_handler)
+
+    def __str__(self):
+        return f"{self.convention}{self.type}"
