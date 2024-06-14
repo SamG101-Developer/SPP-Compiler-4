64cf2d0ba3de8049de92ea5fd6989f9f98d172fd
Sam Gardner <samuelgardner101@gmail.com>
2024-06-14 23:37:19 +0100

    Tidy

--- src/SPPCompiler/SemanticAnalysis/ASTs/ModulePrototypeAst.py
+++ src/SPPCompiler/SemanticAnalysis/ASTs/ModulePrototypeAst.py
@@ -45,7 +45,7 @@ class ModulePrototypeAst(Ast, SemanticAnalyser, LLVMGeneration):
 
     def do_llvm_generation(self, module: llvm_ir.Module, **kwargs) -> Any:
         # Determine the module name and create the llvm module.
-        llvm_module_name = Seq(self.identifier.parts).map(lambda p: p.value).join('.')
+        llvm_module_name = Seq(self.identifier.parts).map(lambda p: p.value).join(".")
         llvm_module = llvm_ir.Module(name=llvm_module_name)
         self.body.do_llvm_generation(llvm_module, **kwargs)
         return llvm_module
