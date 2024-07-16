ae08e73ee113ed364a5ebbc7bd2ca7bd564eb814
Sam Gardner <samuelgardner101@gmail.com>
2024-07-16 15:56:50 +0100

    Fix alternative parser's optional parser

--- src/SPPCompiler/SyntacticAnalysis/ParserAlternateRulesHandler.py
+++ src/SPPCompiler/SyntacticAnalysis/ParserAlternateRulesHandler.py
@@ -1,15 +1,15 @@
 from __future__ import annotations
-from typing import List
+from typing import List, Optional
 
 from SPPCompiler.SemanticAnalysis.ASTs.Meta import Ast
 from SPPCompiler.SyntacticAnalysis.ParserError import ParserError
 from SPPCompiler.SyntacticAnalysis.ParserRuleHandler import ParserRuleHandler
 
 
-class ParserAlternateRulesHandler(ParserRuleHandler):
+class ParserAlternateRulesHandler[T](ParserRuleHandler[T]):
     _parser_rule_handlers: List[ParserRuleHandler]
 
-    def __init__(self, parser: Parser) -> None:
+    def __init__(self, parser: "Parser") -> None:
         super().__init__(parser, None)
         self._parser_rule_handlers = []
 
@@ -27,6 +27,18 @@ class ParserAlternateRulesHandler(ParserRuleHandler):
                 self._parser._index = parser_index
                 continue
         raise ParserError(self._parser._index, "Expected one of the alternatives.")
+
+    def parse_optional(self, save=True) -> Optional[T]:
+        for parser_rule_handler in self._parser_rule_handlers:
+            parser_index = self._parser._index
+            try:
+                ast = parser_rule_handler.parse_optional(save)
+                if ast:
+                    return ast
+            except ParserError:
+                self._parser._index = parser_index
+                continue
+        return None
 
     def __or__(self, that) -> ParserAlternateRulesHandler:
         if not (self._for_alternate and that._for_alternate):
