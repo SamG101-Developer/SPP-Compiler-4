6cd71a15050b911cfe43ba0ea4e1b6cabb24044f
Sam Gardner <samuelgardner101@gmail.com>
2024-08-02 16:13:54 +0100

    Optimize ~50% imports

--- src/SPPCompiler/SemanticAnalysis/ASTs/TypeAst.py
+++ src/SPPCompiler/SemanticAnalysis/ASTs/TypeAst.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import copy
 import hashlib
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import List, Optional
 
 from SPPCompiler.SemanticAnalysis.ASTs.Meta.Ast import Ast
