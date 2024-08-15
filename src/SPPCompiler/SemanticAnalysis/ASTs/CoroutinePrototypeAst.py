baa8527c458be88cad30b55cf7104ba710d23ab6
Sam Gardner <samuelgardner101@gmail.com>
2024-08-15 18:22:17 +0100

    Fixed coroutine return type checking

--- src/SPPCompiler/SemanticAnalysis/ASTs/CoroutinePrototypeAst.py
+++ src/SPPCompiler/SemanticAnalysis/ASTs/CoroutinePrototypeAst.py
@@ -13,10 +13,18 @@ from SPPCompiler.Utils.Sequence import Seq
 class CoroutinePrototypeAst(FunctionPrototypeAst):
     def do_semantic_analysis(self, scope_handler, **kwargs) -> None:
         super().do_semantic_analysis(scope_handler, **kwargs)
-        from SPPCompiler.SemanticAnalysis.ASTs import ReturnStatementAst
+
+        # Ensure the return type is one of the 3 generator return types.
+        # Todo: switch to symbolic equality?
+        if self.return_type.without_generics() not in [
+                CommonTypes.gen_mov().without_generics(),
+                CommonTypes.gen_mut().without_generics(),
+                CommonTypes.gen_ref().without_generics()]:
+            raise SemanticErrors.INVALID_COROUTINE_RETURN_TYPE(self, self.return_type)
 
         # Get the coroutine return type (it is the generic argument in the "Return" type).
-        coroutine_return_type = Seq(self.return_type.parts[-1].generic_arguments.arguments).find(lambda i: i.identifier.parts[-1].to_identifier().value == "Return").type
+        coroutine_return_type = CommonTypes.opt(self.return_type.parts[-1].generic_arguments["Yield"].type)
+        coroutine_return_type.do_semantic_analysis(scope_handler, **kwargs)
 
         # Add the "target-return-type" to the kwargs, so other ASTs can use the function's return type is required.
         # Analyse the body of the function, and then pop the return type from the kwargs.
@@ -24,20 +32,6 @@ class CoroutinePrototypeAst(FunctionPrototypeAst):
         kwargs |= {"coroutine-return-type": coroutine_return_type}
         kwargs |= {"is-coroutine": self.function_token}
         self.body.do_semantic_analysis(scope_handler, inline=True, **kwargs)
-
-        # Ensure the return type is one of the 3 generator return types.
-        if self.return_type.without_generics() not in [
-                CommonTypes.gen_mov().without_generics(),
-                CommonTypes.gen_mut().without_generics(),
-                CommonTypes.gen_ref().without_generics()]:
-            raise SemanticErrors.INVALID_COROUTINE_RETURN_TYPE(coroutine_return_type)
-
-        # Check a "ret" statement exists at the end of the function, as long as it is a subroutine with a non-Void
-        # return type.
-        if (not coroutine_return_type.symbolic_eq(CommonTypes.void(), scope_handler.current_scope)
-                and self.body.members
-                and not isinstance(self.body.members[-1], ReturnStatementAst)):
-            raise SemanticErrors.MISSING_RETURN_STATEMENT(coroutine_return_type, self.body.brace_r_token)
 
         scope_handler.exit_cur_scope()
 
