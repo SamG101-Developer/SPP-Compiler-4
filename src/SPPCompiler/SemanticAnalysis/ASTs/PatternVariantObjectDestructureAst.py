4f045033b6ab697cd93de2564cc3304b27ca485d
Sam Gardner <samuelgardner101@gmail.com>
2024-08-31 01:22:49 +0100

    Merge union destructure logic into object destructure

--- src/SPPCompiler/SemanticAnalysis/ASTs/PatternVariantObjectDestructureAst.py
+++ src/SPPCompiler/SemanticAnalysis/ASTs/PatternVariantObjectDestructureAst.py
@@ -1,11 +1,14 @@
 from dataclasses import dataclass
 from typing import List
+import copy
 
 from SPPCompiler.SemanticAnalysis.ASTs.Meta.Ast import Ast
 from SPPCompiler.SemanticAnalysis.ASTs.Meta.AstMixins import SemanticAnalyser
 from SPPCompiler.SemanticAnalysis.ASTs.Meta.AstUtils import TypeInfer, InferredType
 from SPPCompiler.SemanticAnalysis.ASTs.Meta.AstPrinter import *
+from SPPCompiler.SemanticAnalysis.Utils.CommonTypes import CommonTypes
 from SPPCompiler.SemanticAnalysis.Utils.Scopes import ScopeHandler
+from SPPCompiler.SemanticAnalysis.Utils.SemanticError import SemanticErrors
 from SPPCompiler.Utils.Sequence import Seq
 
 
@@ -64,6 +67,22 @@ class PatternVariantObjectDestructureAst(Ast, SemanticAnalyser, TypeInfer):
         from SPPCompiler.LexicalAnalysis.Tokens import TokenType
         from SPPCompiler.SemanticAnalysis.ASTs import LetStatementInitializedAst, TokenAst
 
+        # Get the condition's symbol (if it exists), and flow-type it.
+        condition_symbol = scope_handler.current_scope.get_symbol(kwargs["condition"])
+        flow_symbol = None
+        if condition_symbol and condition_symbol.type.without_generics().symbolic_eq(CommonTypes.var([]), scope_handler.current_scope):
+
+            # Ensure the composite type belongs to the variant type.
+            union_type = kwargs["condition"].infer_type(scope_handler, **kwargs).type
+            composite_types = union_type.types[-1].generic_arguments.arguments[-1].type.types[-1].generic_arguments.arguments
+            if not any(self.class_type.symbolic_eq(composite_type.type, scope_handler.current_scope) for composite_type in composite_types):
+                raise SemanticErrors.TYPE_DESTRUCTURING_INVALID_TYPE(self.class_type, union_type)
+
+            # Create the flow symbol.
+            flow_symbol = copy.deepcopy(condition_symbol)
+            flow_symbol.type = self.class_type
+            scope_handler.current_scope.add_symbol(flow_symbol)
+
         # Convert the destructuring pattern into a variable, and analyse it.
         bindings = self.convert_to_variable()
         declaration = LetStatementInitializedAst(
@@ -79,6 +98,10 @@ class PatternVariantObjectDestructureAst(Ast, SemanticAnalyser, TypeInfer):
         if symbol is not None:
             symbol.memory_info.ast_consumed = None
             symbol.memory_info.ast_partial_moves = []
+
+        # Remove the flow symbol.
+        if flow_symbol:
+            scope_handler.current_scope.rem_symbol(flow_symbol)
 
     def infer_type(self, scope_handler: ScopeHandler, **kwargs) -> InferredType:
         # The destructuring pattern's type is the class type being destructured into.
