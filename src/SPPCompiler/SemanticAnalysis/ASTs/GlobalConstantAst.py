64758286b754d5aaa57cf2457b98bcd8f1b72505
Sam Gardner <samuelgardner101@gmail.com>
2024-09-13 18:22:43 +0100

    Fixed number of type-analysis problems

--- src/SPPCompiler/SemanticAnalysis/ASTs/GlobalConstantAst.py
+++ src/SPPCompiler/SemanticAnalysis/ASTs/GlobalConstantAst.py
@@ -3,22 +3,23 @@ from dataclasses import dataclass
 from SPPCompiler.SemanticAnalysis.ASTs.Meta.Ast import Ast
 from SPPCompiler.SemanticAnalysis.ASTs.Meta.AstMixins import SemanticAnalyser, SymbolGenerator, PreProcessor, SupScopeLoader
 from SPPCompiler.SemanticAnalysis.ASTs.Meta.AstPrinter import AstPrinter
+from SPPCompiler.SemanticAnalysis.ASTs.Meta.AstUtils import InferredType
 from SPPCompiler.SemanticAnalysis.Utils.Scopes import ScopeHandler
 from SPPCompiler.SemanticAnalysis.Utils.SemanticError import SemanticErrors
 from SPPCompiler.SemanticAnalysis.Utils.Symbols import VariableSymbol
 
 
 @dataclass
-class GlobalConstantAst(Ast, PreProcessor, SupScopeLoader,  SemanticAnalyser, SymbolGenerator):
+class GlobalConstantAst(Ast, PreProcessor, SupScopeLoader, SemanticAnalyser, SymbolGenerator):
     """
     The GlobalConstantAst node is used to represent a global constant (initialized with a value). The constant is a
     single variable, with a literal assigned to it.
 
     Notes:
-        - The constant is a global variable, and is not mutable.
-        - The constant must be initialized with a value.
-        - The constant is auto pinned and cannot be moved.
-        - In the future, more advanced expressions will be supported (via constant functions etc).
+        The constant is a global variable, and is not mutable.
+        The constant must be initialized with a value.
+        The constant is auto pinned and cannot be moved.
+        In the future, more advanced expressions will be supported (via constant functions etc).
 
     Attributes:
         let_keyword: The `let` keyword token.
@@ -29,6 +30,8 @@ class GlobalConstantAst(Ast, PreProcessor, SupScopeLoader,  SemanticAnalyser, Sy
 
     let_keyword: "TokenAst"
     assign_to: "LocalVariableSingleIdentifierAst"
+    colon_token: "Token"
+    type_declaration: "TypeAst"
     assign_token: "TokenAst"
     value: "LiteralAst"
 
@@ -36,33 +39,39 @@ class GlobalConstantAst(Ast, PreProcessor, SupScopeLoader,  SemanticAnalyser, Sy
         s = ""
         s += f"{self.let_keyword.print(printer)}"
         s += f"{self.assign_to.print(printer)} "
+        s += f"{self.colon_token.print(printer)} "
+        s += f"{self.type_declaration.print(printer)} "
         s += f"{self.assign_token.print(printer)} "
         s += f"{self.value.print(printer)}"
         return s
 
-    def pre_process(self, context) -> None:
-        # Dummy implementation required as all module members must implement this method.
-        ...
-
     def generate(self, scope_handler: ScopeHandler) -> None:
-        # Global constants cannot be redifined.
-        # Todo: this might get the same symbols out of the current namespace (exclusive=True)?
-        if scope_handler.current_scope.has_symbol(self.assign_to.identifier):
-            raise SemanticErrors.REDEFINED_GLOBAL_CONSTANT(self.assign_to.identifier, self.let_keyword, scope_handler.current_scope.get_symbol(self.assign_to.identifier).memory_info.ast_initialized)
+        # Global constants cannot be redefined.
+        if existing_symbol := scope_handler.current_scope.parent_module.get_symbol(self.assign_to.identifier, exclusive=True):
+            raise SemanticErrors.REDEFINED_GLOBAL_CONSTANT(self.assign_to.identifier, self.let_keyword, existing_symbol.memory_info.ast_initialized)
 
         # Generate the symbol in the global (or namespaced) scope, and mark it as pinned.
-        variable_symbol = VariableSymbol(name=self.assign_to.identifier, type=self.value.infer_type(scope_handler).type)
-        variable_symbol.memory_info.ast_pins.append(variable_symbol.name)
-        variable_symbol.memory_info.ast_initialized = self
-        scope_handler.current_scope.add_symbol(variable_symbol)
+        symbol = VariableSymbol(name=self.assign_to.identifier, type=self.type_declaration)
+        symbol.memory_info.ast_pins.append(symbol.name)
+        symbol.memory_info.ast_initialized = self
+        scope_handler.current_scope.parent_module.add_symbol(symbol)
 
     def do_semantic_analysis(self, scope_handler: ScopeHandler, **kwargs) -> None:
+        from SPPCompiler.SemanticAnalysis.ASTs import ConventionMovAst
+
         # Ensure the variable is immutable.
         if self.assign_to.is_mutable:
             raise SemanticErrors.MUTABLE_GLOBAL_CONSTANT(self.assign_to.is_mutable)
 
-        # Analyse the value
+        # Analyse the type and value.
+        self.type_declaration.do_semantic_analysis(scope_handler, **kwargs)
         self.value.do_semantic_analysis(scope_handler, **kwargs)
+
+        # Ensure the value matches the type.
+        expected_type = InferredType(convention=ConventionMovAst, type=self.type_declaration)
+        given_type = self.value.infer_type(scope_handler)
+        if not expected_type.symbolic_eq(given_type, scope_handler.current_scope.parent_module):
+            raise SemanticErrors.TYPE_MISMATCH_2(None, self.value, expected_type, given_type, scope_handler)
 
 
 __all__ = ["GlobalConstantAst"]
