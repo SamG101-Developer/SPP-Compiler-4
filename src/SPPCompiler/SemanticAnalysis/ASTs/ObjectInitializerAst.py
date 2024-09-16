04d7ba2c120ac605d950ba29c8aacf9d49afe24d
Sam Gardner <samuelgardner101@gmail.com>
2024-09-16 17:09:48 +0100

    Fix generic inference with object initialization

--- src/SPPCompiler/SemanticAnalysis/ASTs/ObjectInitializerAst.py
+++ src/SPPCompiler/SemanticAnalysis/ASTs/ObjectInitializerAst.py
@@ -30,36 +30,42 @@ class ObjectInitializerAst(Ast, SemanticAnalyser, TypeInfer):
         s += f"{self.arguments.print(printer)}"
         return s
 
-    def do_semantic_analysis(self, scope_handler: ScopeHandler, **kwargs) -> None:
-        from SPPCompiler.SemanticAnalysis.ASTs import IdentifierAst
+    def _genericize_type(self, scope_handler: ScopeHandler, **kwargs) -> "TypeSymbol":
+        from SPPCompiler.SemanticAnalysis.ASTs import IdentifierAst, ObjectInitializerArgumentGroupAst
 
+        Seq(self.arguments.arguments).map(ObjectInitializerArgumentGroupAst.get_argument_value).filter_to_type(ObjectInitializerAst).for_each(lambda a: a._genericize_type(scope_handler, **kwargs))
+
+        base_type_symbol = scope_handler.current_scope.get_symbol(self.class_type.without_generics())
+        generic_infer_source = Seq(self.arguments.arguments).filter(lambda a: isinstance(a.identifier, IdentifierAst)).map(lambda a: (a.identifier, self.arguments.get_argument_value(a).infer_type(scope_handler, **kwargs).type)).dict()
+        generic_infer_target = Seq(base_type_symbol.type.body.members).map(lambda a: (a.identifier, a.type_declaration)).dict()
+
+        # Analyse the type, and ensure that it isn't a generic type being instantiated.
+        self.class_type.do_semantic_analysis(scope_handler, generic_infer_source, generic_infer_target, **kwargs)
+        type_symbol = scope_handler.current_scope.get_symbol(self.class_type)
+        if type_symbol.is_generic:
+            raise SemanticErrors.NON_INSTANTIABLE_TYPE(self.class_type, type_symbol)
+
+        return type_symbol
+
+    def do_semantic_analysis(self, scope_handler: ScopeHandler, **kwargs) -> None:
         # Load generic information for type-analysis.
         base_type_symbol = scope_handler.current_scope.get_symbol(self.class_type.without_generics())
         if not base_type_symbol:
             raise SemanticErrors.UNKNOWN_IDENTIFIER(self.class_type, [], "type")
 
-        generic_infer_from = Seq(self.arguments.arguments).filter(lambda a: isinstance(a.identifier, IdentifierAst)).map(lambda a: (a.identifier, self.arguments.get_argument_value(a).infer_type(scope_handler, **kwargs).type)).dict()
-        generic_map_to = Seq(base_type_symbol.type.body.members).map(lambda a: (a.identifier, a.type_declaration)).dict()
-
-        # Analyse the type, and ensure that it isn't a generic type being instantiated.
-        self.class_type.do_semantic_analysis(scope_handler, generic_infer_from, generic_map_to, **kwargs)
-        type_symbol = scope_handler.current_scope.get_symbol(self.class_type)
-        if type_symbol.is_generic:
-            raise SemanticErrors.NON_INSTANTIABLE_TYPE(self.class_type, type_symbol)
+        type_symbol = self._genericize_type(scope_handler, **kwargs)
 
         # The pre-analysis ensures all the arguments that should be present are, and handles the default/sup special
         # arguments.
         kwargs["attributes"] = Seq(type_symbol.type.body.members).map(lambda a: a.identifier)
         kwargs["class-type"] = self.class_type
         self.arguments.do_semantic_pre_analysis(scope_handler, **kwargs)
-
-        # Type-check the arguments of the object initializer.
         self.arguments.do_semantic_analysis(scope_handler, **kwargs)
 
     def infer_type(self, scope_handler: ScopeHandler, **kwargs) -> InferredType:
         from SPPCompiler.SemanticAnalysis.ASTs import ConventionMovAst
 
-        # The returning type of the object initializer is the type of the object being constructed.
+        # The class type (which should have been generic-substituted) is the inferred type.
         return InferredType(convention=ConventionMovAst, type=self.class_type)
 
 
